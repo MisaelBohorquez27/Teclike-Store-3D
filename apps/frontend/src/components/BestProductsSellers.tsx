f2cd920a6e09cbb182e86b374136a7261f358e28
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2025-07-28 17:18:37 -0500

    fix(Fixing the components)

--- apps/frontend/src/components/BestProductsSellers.tsx
+++ apps/frontend/src/components/BestProductsSellers.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import mouse from "../../public/products/mouse-x11.png";
-import Button from "./Button";
+import Button from "@/components/ui/PagesButtons";
 
 type Product = {
   id: number;
