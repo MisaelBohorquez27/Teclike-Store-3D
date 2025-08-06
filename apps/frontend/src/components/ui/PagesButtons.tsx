ac6974d7d4d197f38b115f64f4703edb4088295d
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2025-08-06 14:23:35 -0500

    add(Adding responsive styles in Navbar)

--- apps/frontend/src/components/ui/PagesButtons.tsx
+++ apps/frontend/src/components/ui/PagesButtons.tsx
@@ -5,7 +5,7 @@ import { motion, useReducedMotion } from "framer-motion";
 
 interface ButtonProps extends ComponentPropsWithoutRef<typeof motion.button> {
   variant?: "primary" | "secondary" | "addCart" | "outline" | "submit";
-  size?: "xs" | "2xs" | "s" | "m" | "xl" | "2xl";
+  size?: "2xs" | "xs" | "s" | "m" | "xl" | "2xl";
 }
 
 const Button: FC<ButtonProps> = ({
@@ -19,7 +19,7 @@ const Button: FC<ButtonProps> = ({
 
   return (
     <motion.button
-      whileHover={{ scale: prefersReducedMotion ? 1 : 1.05 }}
+      whileHover={{ scale: prefersReducedMotion ? 1 : 1.02 }}
       whileTap={{ scale: prefersReducedMotion ? 1 : 0.98 }}
       className={cn(
         "rounded-lg font-semibold transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
@@ -33,8 +33,8 @@ const Button: FC<ButtonProps> = ({
           "bg-transparent text-[#0F2C59] border-2 border-[#0F2C59] hover:bg-[#0F2C59] hover:text-[#FAF9F6]",
         variant === "submit" &&
           "bg-[#3778d4] text-[#FAF9F6] hover:bg-[#5ca0ff] border-2 border-transparent",
-        size === "2xs" && "px-2 py-1 text-2xs font-extralight",
-        size === "xs" && "px-3 py-2 md:text-xs font-medium",
+        size === "2xs" && "px-2 py-1 text-xs font-extralight",
+        size === "xs" && "px-3 py-2 md:text-s font-medium",
         size === "s" && "px-4 py-3 md:text-sm font-medium",
         size === "m" && "px-5 py-4 md:text-base font-semibold",
         size === "xl" && "px-5 py-4 md:text-lg font-semibold",
