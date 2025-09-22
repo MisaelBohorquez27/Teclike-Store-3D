b30132cbeeced49af9e568c5bdf72791437c1885
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2025-09-21 20:44:10 -0500

    ref(TrendingProducts Component was refactored)

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { HeroBanner } from "@/components/HeroBanner";
 import { TrendingProducts } from "@/components/TrendingProducts/TrendingProducts";
 import { CustomerReviews } from "@/components/CustomerReview/CustomerReviews";
 import { BenefitsGrid } from "@/components/Benefits/BenefitsGrid";
-import { FollowUs } from "@/components/FollowUs";
+import { FollowUs } from "@/components/FollowUs/FollowUs";
 import { TopProductSellPage } from "@/components/BestSellerWeek/BSWPage";
 
 export default function Home() {
